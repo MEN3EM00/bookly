198a017e564124071e72717e4f8518649f6d2e8f
adham <men3em4@gmail.com>
2025-04-30 16:52:38 +0300

    add custom list item widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:bookly/core/constants.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/route_manager.dart';
 
 void main() {
-  runApp(const BooklyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown, // Allow upside-down portrait
+  ]).then((_) {
+    runApp(const BooklyApp());
+  });
 }
 
 class BooklyApp extends StatelessWidget {
